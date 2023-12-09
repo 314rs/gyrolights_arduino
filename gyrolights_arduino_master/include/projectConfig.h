eb78656e949a8a4d2e7863611f0fe04c73397d72
314rs <lars.borntraeger@gmx.de>
2023-12-09 17:23:30 +0100

    move localtask to projectConfig.h

--- gyrolights_arduino_master/include/projectConfig.h
+++ gyrolights_arduino_master/include/projectConfig.h
@@ -30,7 +30,7 @@ namespace conf
 const char WIFI_AP_SSID_PREFIX[] = "ESP32_"; ///< first half of the access point ssid. second half are the last 6 characters of the mac address in hex.
 const char WIFI_AP_PW[] = "password"; ///< password of the configuration access point
 
-const int NUM_LEDS_PER_STRIP = 5; ///< number of leds per individual strip
+const int NUM_LEDS_PER_STRIP = 21; ///< number of leds per individual strip
 const int NUM_STRIPS = 1; ///< number of led strips
 const uint8_t MAX_BRIGHTNESS = 80; ///< maximum led brightness value. from 0 (always off) to 255 (maximum possible brightness).
 
@@ -44,6 +44,8 @@ const int UNIVERSE_COUNT = 1; ///< Total number of Universes to listen for, star
 
 const char BLE_SERVICE_UUID[] = "018c4715-a90b-7ff1-8c4c-aeed790b0a0a";
 const char BLE_CHARACTERISTIC_UUID[] = "018c4715-f39b-7e59-8928-4a8eda02c6a5";
+
+const char BLE_MASTER_NAME[] = "ESP_32"; ///< this has to be the same name for a pair of lights
 
 
 /* -------------------------------------------------------------------------- */
@@ -165,11 +167,173 @@ DEFINE_GRADIENT_PALETTE(heatmap_fire_) {
     };
 
 DEFINE_GRADIENT_PALETTE(heatmap_test_) {
-        0,   0,     0, 100, // blue
-        255, 255,   0, 255, // yellow
+           0,   0,   0, 100, // blue
+         255, 255,   0, 255, // yellow
 };
 
 DEFINE_GRADIENT_PALETTE(heatmap_tooff_) {
-          0,   255,     200, 0, // blue
-        255, 0,    0,    0, // yellow
+           0, 255, 200,   0, // blue
+         255,   0,   0,   0, // yellow
 };
+
+
+static CRGB leds[conf::NUM_STRIPS][conf::NUM_LEDS_PER_STRIP];
+
+static int8_t rotaryswitch = -1;
+
+
+
+
+/**
+ * @brief handle local tasks
+ * 
+ */
+void localtask(void*) {
+    ESP_LOG_LEVEL(ESP_LOG_DEBUG, __FUNCTION__, "%s started", __FUNCTION__);
+    // case 0
+    int16_t rxvalues[7];
+    static uint8_t r, g, b;
+    const float alpha = 1.0/16.0;
+
+    // case 6
+    unsigned long time_now, time_prev;
+    time_now = time_prev = millis();
+    float norm_accel_max = 0, norm_reading = 0, norm_prev = 0;
+    static float norm_accel = 0;
+    CRGBPalette16 fire = heatmap_fire_;
+    CRGBPalette16 test = heatmap_test_;
+    CRGBPalette16 tooff = heatmap_tooff_;
+    float jerk = 0;
+    static float jerk_prev = 0, jerk_max = 0, jerk_min = 0;
+    float directednormaccel = 0;
+    uint8_t brightness = 0;
+
+    
+
+
+    while (1) {
+        ESP_LOG_LEVEL(ESP_LOG_VERBOSE, __FUNCTION__, "high watermark: %d, rotaryswitch: %d", uxTaskGetStackHighWaterMark(NULL), rotaryswitch);
+        FastLED.show(conf::MAX_BRIGHTNESS);
+        /**
+         * @todo 
+         * line @lineinfo: replace  switch with array of function pointers and
+         * put array in projectConfig.h
+         */
+        switch (rotaryswitch) {
+        case 0:
+            readGyro(rxvalues);
+            r = ((1-alpha) * r ) + (alpha * (abs(rxvalues[0]) >> 7));
+            g = ((1-alpha) * g ) + (alpha * (abs(rxvalues[1]) >> 7));
+            b = ((1-alpha) * b ) + (alpha * (abs(rxvalues[2]) >> 7));
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB(r, g, b));
+            //FastLED.show();
+            break;
+        case 1:
+            /* ---------------------------------- jerk ---------------------------------- */
+            time_now = millis();
+            norm_reading = directedAccelNorm();
+            jerk = (norm_reading - norm_prev)/(time_now-time_prev);
+            time_prev = time_now;
+            norm_prev = norm_reading;
+            // bounds
+            jerk_max = jerk_max * 0.99;
+            jerk_min = jerk_min * 0.99;
+            if (jerk > jerk_max) jerk_max = jerk;
+            else if (jerk < jerk_min) jerk_min = jerk;
+            ESP_LOG_LEVEL(ESP_LOG_DEBUG, __FUNCTION__, "jerk: %f", jerk);
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::White);
+            // FastLED.show(mapfb(jerk, jerk_min, jerk_max, 0, 255));
+            //FastLED.show(255 * (jerk > 100.0));
+            break;
+        case 2:
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Green);
+            readGyro(rxvalues);
+            ESP_LOG_LEVEL(ESP_LOG_INFO, "readGyro", "accelx: %7d, accely: %7d, accelz: %7d, temp: %7d, gyrox: %7d, gyroy: %7d, gyroz: %7d\r", (rxvalues[0]), (rxvalues[1]), (rxvalues[2]), (rxvalues[3]), (rxvalues[4]), (rxvalues[5]), (rxvalues[6]));
+
+            //FastLED.show(conf::MAX_BRIGHTNESS);
+            //vTaskSuspend(NULL);
+            break;
+        case 3:
+            directednormaccel = directedAccelNorm();
+            if (directednormaccel < -10000) {
+                fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Green);
+            } else {
+                fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Black);
+            }
+            ESP_LOG_LEVEL(ESP_LOG_INFO, "accel", "acceleration norm: %f\r", directednormaccel);
+            //FastLED.show(conf::MAX_BRIGHTNESS);
+            //vTaskSuspend(NULL);
+            break;
+        case 4:
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::YellowGreen);
+            //FastLED.show(conf::MAX_BRIGHTNESS);
+            //vTaskSuspend(NULL);
+            break;
+        case 5:
+            readGyro(rxvalues);
+            
+            norm_reading = sqrtf(pow(rxvalues[0], 2) + pow(rxvalues[1], 2) + pow(rxvalues[2], 2)) - conf::G_VAL;
+            if (norm_accel_max < norm_accel) 
+                norm_accel_max = norm_accel;
+            if (norm_reading < norm_accel + 200) {
+                norm_accel = (norm_reading * alpha) +  (norm_accel * (1.0 - alpha));
+            } else {
+                norm_accel = norm_reading;
+            }
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, ColorFromPalette(tooff, 0xff * (norm_accel/norm_accel_max)));
+            //FastLED.show();
+            break;
+        case 6:
+            readGyro(rxvalues);
+            
+            norm_reading = sqrtf(pow(rxvalues[0], 2) + pow(rxvalues[1], 2) + pow(rxvalues[2], 2)) - conf::G_VAL;
+            if (norm_accel_max < norm_accel) 
+                norm_accel_max = norm_accel;
+            if (norm_reading < norm_accel + 200) {
+                norm_accel = (norm_reading * alpha) +  (norm_accel * (1.0 - alpha));
+            } else {
+                norm_accel = norm_reading;
+            }
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, ColorFromPalette(fire, 0xff * (norm_accel/norm_accel_max)));
+            //FastLED.show();
+            break;
+        case 7:
+            readGyro(rxvalues);
+            ESP_LOG_LEVEL(ESP_LOG_INFO, "readGyro", "accelx: %7d, accely: %7d, accelz: %7d, temp: %7d, gyrox: %7d, gyroy: %7d, gyroz: %7d\r", (rxvalues[0]), (rxvalues[1]), (rxvalues[2]), (rxvalues[3]), (rxvalues[4]), (rxvalues[5]), (rxvalues[6]));
+            directednormaccel = directedAccelNorm();
+            if (directednormaccel < -10000) {
+                brightness = conf::MAX_BRIGHTNESS;
+                fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Cyan);
+            } else {
+                brightness = brightness * 0.95;
+            }
+            ESP_LOG_LEVEL(ESP_LOG_INFO, "accel", "acceleration norm: %f\r", directednormaccel);
+            //FastLED.show();
+            //vTaskSuspend(NULL);
+            break;
+        
+        case 8:
+            readGyro(rxvalues);
+            
+            norm_reading = sqrtf(pow(rxvalues[0], 2) + pow(rxvalues[1], 2) + pow(rxvalues[2], 2)) - conf::G_VAL;
+            if (norm_accel_max < norm_accel) 
+                norm_accel_max = norm_accel;
+            if (norm_reading < norm_accel + 200) {
+                norm_accel = (norm_reading * alpha) +  (norm_accel * (1.0 - alpha));
+            } else {
+                norm_accel = norm_reading;
+            }
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, ColorFromPalette(test, 0xff * (norm_accel/norm_accel_max)));
+            
+            break;
+
+        
+        default:
+            fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Black);
+            //FastLED.show();
+            //vTaskSuspend(NULL);
+            break;
+        }
+        vTaskDelay(5);
+    }
+}
--- gyrolights_arduino_master/src/main.cpp
+++ gyrolights_arduino_master/src/main.cpp
@@ -7,6 +7,7 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 #include <FastLED.h>
 #include <ESPAsyncE131.h>
@@ -18,7 +19,6 @@
 
 #include "projectConfig.h"
 
-static CRGB leds[conf::NUM_STRIPS][conf::NUM_LEDS_PER_STRIP];
 
 WiFiUDP Udp;
 
@@ -34,7 +34,6 @@ WiFiServer Server(23);
 
 static SemaphoreHandle_t stateMutex;
 bool switchRF = false;
-static int8_t rotaryswitch = -1;
 
 static button_t rotarySwitch[conf::NUM_PINS_ROTARY_SWITCH];
 static button_t rfSwitch;
@@ -42,6 +41,8 @@ static button_t rfSwitch;
 ESPAsyncE131 e131(conf::UNIVERSE_COUNT);
 
 BLECharacteristic *pCharacteristic = nullptr;
+BLEDescriptor* pDescriptor = new BLE2902();
+static bool deviceConnected = false;
 
 
 
@@ -163,166 +164,7 @@ float mapfb(float in, float in_min, float in_max, float out_min, float out_max) 
     return (in - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-/**
- * @brief handle local tasks
- * 
- */
-void localtask(void*) {
-    esp_log_write(ESP_LOG_DEBUG, __FUNCTION__, "%s started", __FUNCTION__);
-    // case 0
-    int16_t rxvalues[7];
-    static uint8_t r, g, b;
-    const float alpha = 1.0/16.0;
 
-    // case 6
-    unsigned long time_now, time_prev;
-    time_now = time_prev = millis();
-    float norm_accel_max = 0, norm_reading = 0, norm_prev = 0;
-    static float norm_accel = 0;
-    CRGBPalette16 fire = heatmap_fire_;
-    CRGBPalette16 test = heatmap_test_;
-    CRGBPalette16 tooff = heatmap_tooff_;
-    float jerk = 0;
-    static float jerk_prev = 0, jerk_max = 0, jerk_min = 0;
-    float directednormaccel = 0;
-    uint8_t brightness = 0;
-
-    
-
-
-    while (1) {
-        // ESP_LOG_LEVEL(ESP_LOG_VERBOSE, __FUNCTION__, "high watermark: %d, rotaryswitch: %d", uxTaskGetStackHighWaterMark(NULL), rotaryswitch);
-        vTaskDelay(5);
-        continue;
-        readGyro(rxvalues);
-        ESP_LOG_LEVEL(ESP_LOG_INFO, "readGyro", "accelx: %7d, accely: %7d, accelz: %7d, temp: %7d, gyrox: %7d, gyroy: %7d, gyroz: %7d\r", (rxvalues[0]), (rxvalues[1]), (rxvalues[2]), (rxvalues[3]), (rxvalues[4]), (rxvalues[5]), (rxvalues[6]));
-        directednormaccel = directedAccelNorm();
-        if (directednormaccel < -10000) {
-            brightness = conf::MAX_BRIGHTNESS;
-            fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Cyan);
-        } else {
-            brightness = brightness * 0.95;
-        }
-        ESP_LOG_LEVEL(ESP_LOG_INFO, "accel", "acceleration norm: %f\r", directednormaccel);
-        FastLED.show(brightness);
-        /**
-         * @todo 
-         * line @lineinfo: replace  switch with array of function pointers and
-         * put array in projectConfig.h
-         */
-        // switch (rotaryswitch) {
-        // case 0:
-        //     readGyro(rxvalues);
-        //     r = ((1-alpha) * r ) + (alpha * (abs(rxvalues[0]) >> 7));
-        //     g = ((1-alpha) * g ) + (alpha * (abs(rxvalues[1]) >> 7));
-        //     b = ((1-alpha) * b ) + (alpha * (abs(rxvalues[2]) >> 7));
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB(r, g, b));
-        //     //FastLED.show();
-        //     break;
-        // case 1:
-        //     /* ---------------------------------- jerk ---------------------------------- */
-        //     time_now = millis();
-        //     norm_reading = directedAccelNorm();
-        //     jerk = (norm_reading - norm_prev)/(time_now-time_prev);
-        //     time_prev = time_now;
-        //     norm_prev = norm_reading;
-        //     // bounds
-        //     jerk_max = jerk_max * 0.99;
-        //     jerk_min = jerk_min * 0.99;
-        //     if (jerk > jerk_max) jerk_max = jerk;
-        //     else if (jerk < jerk_min) jerk_min = jerk;
-        //     ESP_LOG_LEVEL(ESP_LOG_DEBUG, __FUNCTION__, "jerk: %f", jerk);
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::White);
-        //     // FastLED.show(mapfb(jerk, jerk_min, jerk_max, 0, 255));
-        //     //FastLED.show(255 * (jerk > 100.0));
-        //     break;
-        // case 2:
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Cyan);
-        //     readGyro(rxvalues);
-        //     ESP_LOG_LEVEL(ESP_LOG_INFO, "readGyro", "accelx: %7d, accely: %7d, accelz: %7d, temp: %7d, gyrox: %7d, gyroy: %7d, gyroz: %7d\r", (rxvalues[0]), (rxvalues[1]), (rxvalues[2]), (rxvalues[3]), (rxvalues[4]), (rxvalues[5]), (rxvalues[6]));
-
-        //     //FastLED.show(conf::MAX_BRIGHTNESS);
-        //     //vTaskSuspend(NULL);
-        //     break;
-        // case 3:
-        //     directednormaccel = directedAccelNorm();
-        //     if (directednormaccel < -10000) {
-        //         fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Pink);
-        //     } else {
-        //         fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Black);
-        //     }
-        //     ESP_LOG_LEVEL(ESP_LOG_INFO, "accel", "acceleration norm: %f\r", directednormaccel);
-        //     //FastLED.show(conf::MAX_BRIGHTNESS);
-        //     //vTaskSuspend(NULL);
-        //     break;
-        // case 4:
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::SeaGreen);
-        //     //FastLED.show(conf::MAX_BRIGHTNESS);
-        //     //vTaskSuspend(NULL);
-        //     break;
-        // case 5:
-        //     readGyro(rxvalues);
-            
-        //     norm_reading = sqrtf(pow(rxvalues[0], 2) + pow(rxvalues[1], 2) + pow(rxvalues[2], 2)) - conf::G_VAL;
-        //     if (norm_accel_max < norm_accel) 
-        //         norm_accel_max = norm_accel;
-        //     if (norm_reading < norm_accel + 200) {
-        //         norm_accel = (norm_reading * alpha) +  (norm_accel * (1.0 - alpha));
-        //     } else {
-        //         norm_accel = norm_reading;
-        //     }
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, ColorFromPalette(tooff, 0xff * (norm_accel/norm_accel_max)));
-        //     //FastLED.show();
-        //     break;
-        // case 6:
-        //     readGyro(rxvalues);
-            
-        //     norm_reading = sqrtf(pow(rxvalues[0], 2) + pow(rxvalues[1], 2) + pow(rxvalues[2], 2)) - conf::G_VAL;
-        //     if (norm_accel_max < norm_accel) 
-        //         norm_accel_max = norm_accel;
-        //     if (norm_reading < norm_accel + 200) {
-        //         norm_accel = (norm_reading * alpha) +  (norm_accel * (1.0 - alpha));
-        //     } else {
-        //         norm_accel = norm_reading;
-        //     }
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, ColorFromPalette(fire, 0xff * (norm_accel/norm_accel_max)));
-        //     //FastLED.show();
-        //     break;
-        // case 7:
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::SeaGreen);
-        //     //FastLED.show();
-        //     //vTaskSuspend(NULL);
-        //     break;
-        
-        // case 8:
-        //     readGyro(rxvalues);
-            
-        //     norm_reading = sqrtf(pow(rxvalues[0], 2) + pow(rxvalues[1], 2) + pow(rxvalues[2], 2)) - conf::G_VAL;
-        //     if (norm_accel_max < norm_accel) 
-        //         norm_accel_max = norm_accel;
-        //     if (norm_reading < norm_accel + 200) {
-        //         norm_accel = (norm_reading * alpha) +  (norm_accel * (1.0 - alpha));
-        //     } else {
-        //         norm_accel = norm_reading;
-        //     }
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, ColorFromPalette(test, 0xff * (norm_accel/norm_accel_max)));
-        //     //taskENTER_CRITICAL(&my_spinlock);
-        //     // We are now in a critical section
-        //     //FastLED.show(conf::MAX_BRIGHTNESS);
-        //     //taskEXIT_CRITICAL(&my_spinlock);
-            
-        //     break;
-
-        
-        // default:
-        //     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Black);
-        //     //FastLED.show();
-        //     vTaskSuspend(NULL);
-        //     break;
-        // }
-        vTaskDelay(5);
-    }
-}
 
 /**
  * @brief callback for the RF switch
@@ -379,6 +221,17 @@ void callbackRotaryswitch(button_t *btn, button_state_t state) {
     }
 }
 
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
 
 /**
  * @brief Start WiFi and also e131 operation mode.
@@ -400,11 +253,14 @@ void stopWifi() {
 }
 
 void startBLE() {
-    BLEDevice::init("ESP32");
+    BLEDevice::init(conf::BLE_MASTER_NAME);
     BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
     BLEService *pService = pServer->createService(conf::BLE_SERVICE_UUID);
     pCharacteristic = pService->createCharacteristic(conf::BLE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
     pCharacteristic->setValue("Value: Hello World!");
+    pDescriptor->setValue("Oakleaf | Gyro-Mode");
+    pCharacteristic->addDescriptor(pDescriptor);
     pService->start();
     BLEAdvertising *pAdvertising = pServer->getAdvertising();
     pAdvertising->addServiceUUID(conf::BLE_SERVICE_UUID);
@@ -427,20 +283,8 @@ void setup() {
     esp_log_level_set("*", ESP_LOG_DEBUG);
     //esp_log_level_set("printGyro", ESP_LOG_ERROR);
 
-    
-    // e131
-    delay(100);
-    //ESP_ERROR_CHECK(!e131.begin(E131_MULTICAST, conf::UNIVERSE, conf::UNIVERSE_COUNT));
-    // e131
-
-    startBLE();
-
-    telnet.onInputReceived(onTelnetInput);
-    telnet.begin();
-    
-
     // config mpu6050 gyro sensor
-    /* i2c_config_t conf = {
+    i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = conf::PIN_MPU6050_SDA,
         .scl_io_num = conf::PIN_MPU6050_SCL,
@@ -454,18 +298,22 @@ void setup() {
     ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_write_to_device(I2C_NUM_0, conf::I2C_GYRO_ADDR, setuptx, sizeof(setuptx), conf::I2C_TIMEOUT_MS/portTICK_RATE_MS));
     static const uint8_t acc_range[] = {0x1C, (conf::GYRO_ACCEL_SENSITIVITY << 3)}; // range of accelerometer   // register for gyro: 0x1b
     ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_write_to_device(I2C_NUM_0, conf::I2C_GYRO_ADDR, acc_range, sizeof(acc_range), conf::I2C_TIMEOUT_MS/portTICK_RATE_MS));
- */
+
+    
+    // e131
+    delay(100);
+    //ESP_ERROR_CHECK(!e131.begin(E131_MULTICAST, conf::UNIVERSE, conf::UNIVERSE_COUNT));
+   
+
+
+    startBLE();
+
+
     // FastLED
     // black
-    // FastLED.addLeds<APA102, conf::PIN_LED_DATA_1, conf::PIN_LED_CLOCK,  BGR>(leds[0], conf::NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<APA102, conf::PIN_LED_DATA_1, conf::PIN_LED_CLOCK,  BGR>(leds[0], conf::NUM_LEDS_PER_STRIP);
     
-    // white
-    FastLED.addLeds<WS2812B, conf::PIN_LED_DATA_1, GRB>(leds[0], conf::NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<WS2812B, conf::PIN_LED_DATA_2, GRB>(leds[0], conf::NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<WS2812B, conf::PIN_LED_DATA_3, GRB>(leds[0], conf::NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<WS2812B, conf::PIN_LED_DATA_4, GRB>(leds[0], conf::NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<WS2812B, conf::PIN_LED_DATA_5, GRB>(leds[0], conf::NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<WS2812B, conf::PIN_LED_DATA_6, GRB>(leds[0], conf::NUM_LEDS_PER_STRIP); 
+    
     fill_solid(*leds, conf::NUM_LEDS_TOTAL, CRGB::Black);
     FastLED.show();
 
